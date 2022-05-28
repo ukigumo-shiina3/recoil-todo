499d10a6c690b48c72ce203f4bef7b7927d8fe08
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-05-28 23:00:47 +0900

    ✨ atomの設定

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,25 @@
+import { atom, useRecoilValue } from "recoil";
+
+const todoListState = atom({
+  key: "todoListState",
+  default: [
+    {
+      id: 0,
+      title: "メール送信",
+      isComplete: false,
+    },
+  ],
+});
+
 function TodoList() {
+  // useRecoilValue: atomの値のみを取得する。Read Onlyなので状態の取得はできるが更新を行うことはできない
+  const todoList = useRecoilValue(todoListState);
   return (
     <>
       <h1>RecoilによるTodoアプリ</h1>
+      {todoList.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
     </>
   );
 }
