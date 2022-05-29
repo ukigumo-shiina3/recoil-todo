f51b11ac22ed524d97193783f4aa1f8f578ff10b
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-05-29 10:12:38 +0900

    🚚 ファイルの分割

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,6 @@
-import { atom, selector, useRecoilValue } from "recoil";
-
-const todoListState = atom({
-  key: "todoListState",
-  default: [
-    {
-      id: 0,
-      title: "メール送信",
-      isComplete: false,
-    },
-  ],
-});
-
-const todoListStatsState = selector({
-  key: "todoListStatsState",
-  get: ({ get }) => {
-    const todoList = get(todoListState);
-    const totalNum = todoList.length;
-    return totalNum;
-  },
-});
+import { useRecoilValue } from "recoil";
+import { todoListState } from "../atom";
+import { todoListStatsState } from "../selector";
 
 function TodoList() {
   // useRecoilValue: atomの値のみを取得する。Read Onlyなので状態の取得はできるが更新を行うことはできない
