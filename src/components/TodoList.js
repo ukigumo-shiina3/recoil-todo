1e40deb3fdf32b0f49806c76cb034160fef2454c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-05-29 09:53:56 +0900

    ✨ selectorの設定

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import { atom, useRecoilValue } from "recoil";
+import { atom, selector, useRecoilValue } from "recoil";
 
 const todoListState = atom({
   key: "todoListState",
@@ -11,12 +11,25 @@ const todoListState = atom({
   ],
 });
 
+const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    return totalNum;
+  },
+});
+
 function TodoList() {
   // useRecoilValue: atomの値のみを取得する。Read Onlyなので状態の取得はできるが更新を行うことはできない
   const todoList = useRecoilValue(todoListState);
+  const totalNum = useRecoilValue(todoListStatsState);
   return (
     <>
       <h1>RecoilによるTodoアプリ</h1>
+      <ul>
+        <li>Todoの登録数:{totalNum}</li>
+      </ul>
       {todoList.map((item) => (
         <div key={item.id}>{item.title}</div>
       ))}
