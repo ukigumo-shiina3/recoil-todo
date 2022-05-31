e22d8fccb0008bf2f3b6e6f0f7a6f8bbd57fba0d
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-05-31 11:46:11 +0900

    ✨ 完了・未完了の切り替え

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,8 +13,22 @@ function TotoItem({ item }) {
     setTodoList(newTodoList);
   };
 
+  const toggleItemCompletion = () => {
+    const index = todoList.findIndex((listItem) => listItem.id === item.id);
+    const newTodoList = [
+      ...todoList.slice(0, index),
+      { ...item, isComplete: !item.isComplete },
+      ...todoList.slice(index + 1),
+    ];
+    setTodoList(newTodoList);
+  };
+
   return (
     <div>
+      <button onClick={toggleItemCompletion}>
+        {item.isComplete ? "完" : "未"}
+      </button>
+
       {item.title}
       <span onClick={deleteItem} style={{ cursor: "pointer" }}>
         X
