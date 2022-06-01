55eece6e88428c426e7eb44822c428053bc7137d
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-06-01 11:58:09 +0900

    ✨ 未完了数、完了数の表示

--- src/components/TodoListStats.js
+++ src/components/TodoListStats.js
@@ -2,10 +2,13 @@ import { useRecoilValue } from "recoil";
 import { todoListStatsState } from "../selector";
 
 function TodoListStats() {
-  const totalNum = useRecoilValue(todoListStatsState);
+  const { totalNum, totalCompletedNum, totalUncompletedNum } =
+    useRecoilValue(todoListStatsState);
   return (
     <ul>
       <li>Todoの登録数:{totalNum}</li>
+      <li>完了の数:{totalCompletedNum}</li>
+      <li>未完了の数:{totalUncompletedNum}</li>
     </ul>
   );
 }
--- src/selector.js
+++ src/selector.js
@@ -6,6 +6,14 @@ export const todoListStatsState = selector({
   get: ({ get }) => {
     const todoList = get(todoListState);
     const totalNum = todoList.length;
-    return totalNum;
+    // filter関数を利用してisCompleteプロパティがtrueの完了済みのitemのみ取得してlengthで配列の大きさを計算
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    // totalNumからtotalCompletedNumを引いて未完了のitemの数を取得
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+    };
   },
 });
